0628b6c613b421b2f1bf5741ccab6b97ee7e1050
Maurice Wahba <mauricew@users.noreply.github.com>
2011-02-24 02:57:04 +0000

    Version 0.1.1 candidate

--- GSL Caster Bar/Properties/AssemblyInfo.cs
+++ GSL Caster Bar/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GSL Caster Bar")]
+[assembly: AssemblyTitle("GSLBar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("[twisted] broadcasting")]
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.1")]
